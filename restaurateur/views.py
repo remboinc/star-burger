8e156d2b492e7c6978adb11a0f3320bdf29e721e
Ekaterina Ryabova <58534082+remboinc@users.noreply.github.com>
2025-02-17 22:11:30 +0300

    Update views.py

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
 
 
-from foodcartapp.models import Product, Restaurant, OrderItem
+from foodcartapp.models import Product, Restaurant, OrderItem, Order
 
 
 class Login(forms.Form):
@@ -93,17 +93,19 @@ def view_restaurants(request):
 
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
-    orders = OrderItem.objects.select_related('order',).all()
+    orders = Order.objects.prefetch_related('items__product').all()
 
     order_summary = []
-    for order_item in orders:
-        order_items = OrderItem.objects.filter(order_id=order_item.order.id).annotate(
-            total_price=F('product__price') * F('quantity')
-        )
-        total_price = order_items.aggregate(total=Sum('total_price'))['total']
+    for order in orders:
+        order_items = order.items.all()
+        if not order_items:
+            continue
+        total_price = sum(item.product.price * item.quantity for item in order_items)
+
         order_summary.append({
-            'order': order_item.order,
-            'total_price': total_price
+            'order': order,
+            'total_price': total_price,
+            'order_items': order_items
         })
 
     return render(request, 'order_items.html', {'order_summary': order_summary})
